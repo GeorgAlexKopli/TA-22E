d293618327e479b8897baa8e307ae7175580c2b1
GeorgAlexKopli <113168730+GeorgAlexKopli@users.noreply.github.com>
2022-09-09 10:42:25 +0300

    Add files via upload

---
+++ checkTemp.js
@@ -0,0 +1,8 @@
+const temperature = 45
+if (temperature >= 21 && temperature <= 40) {
+    console.log("0"); //Temp on paras
+} if (temperature <= 20 && temperature >= 0) {
+    console.log("-1"); //Temp on liiga madal
+} if (temperature >= 41 && temperature <= 60) {
+    console.log("+1"); //Temp on liiga kõrge
+} 
